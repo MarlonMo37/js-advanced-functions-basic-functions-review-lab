73d3d030bf3a48162cce4f209604a76c5785c1e3
MarlonMoreno <marlonmoreno894@gmail.com>
2021-05-19 15:59:00 -0500

    commit tree b15120109ebe1cf41c555003a244590905a11890

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(string = "*") {
+    return function(thing = "special") {
+        return `You are ${string}${thing}${string}!`
+    }
+}
+
+const Calculator = {
+    add: function(a, b) {
+        return a + b
+    },
+    subtract: function(a, b) {
+        return a - b
+    },
+    multiply: function(a, b) {
+        return a * b
+    },
+    divide: function(a, b) {
+        return a / b
+    }
+}
+
+function actionApplyer(sInteger, functions) {
+    let og = sInteger
+
+    for (let i = 0; i < functions.length; i++){
+        og = functions[i](og)
+    }
+    return og
+}
